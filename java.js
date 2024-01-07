79564d416d78dcff3ca5db4e216390c243a4da93
ritakerkovits <153216366+ritakerkovits@users.noreply.github.com>
2024-01-07 18:41:08 +0100

    Add files via upload
    basics

---
+++ java.js
@@ -0,0 +1,237 @@
+function vida(){    
+    document.getElementById("info-vida").innerHTML = "<p>Évjárat: 2018</p><p>Szőlőfajta: Merlot</p><p>Alkoholtartalom: 14%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+}
+function mesz(){    
+    document.getElementById("info-mesz").innerHTML = "<p>Évjárat: 2017</p><p>Szőlőfajta:</p> <p> Cabarnet Sauvignon</p><p>Alkoholtartalom: 14,5%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+}
+function sauskabor(){    
+    document.getElementById("info-sauskabor").innerHTML = "<p>Évjárat: 2018</p><p>Szőlőfajta: Vörös házasítás </p><p>Alkoholtartalom: 13,5%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+
+}
+function gere(){    
+    document.getElementById("info-gere").innerHTML = "<p>Évjárat: 2016</p><p>Szőlőfajta:</p><p> Cabarnet Sauvignon</p><p>Alkoholtartalom: 13,5%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+
+}
+function bonsai(){    
+    document.getElementById("info-bonsai").innerHTML = "<p>Évjárat: 2019</p><p>Szőlőfajta: Kadarka</p><p>Alkoholtartalom: 13%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+
+}
+function aldas(){    
+    document.getElementById("info-aldas").innerHTML = "<p>Évjárat: 2019</p><p>Szőlőfajta: Bikavér</p><p>Alkoholtartalom: 14,5%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+}
+
+function nonevida(){
+    document.getElementById("info-vida").innerHTML= "";
+}
+function nonemesz(){
+    document.getElementById("info-mesz").innerHTML= "";
+}
+
+function nonesauskabor(){
+    document.getElementById("info-sauskabor").innerHTML= "";
+}
+
+function nonegere(){
+    document.getElementById("info-gere").innerHTML= "";
+}
+function nonebonsai(){
+    document.getElementById("info-bonsai").innerHTML= "";
+}
+
+function nonealdas(){
+    document.getElementById("info-aldas").innerHTML= "";
+}
+
+
+
+
+//fehérborok
+
+function figu(){
+    document.getElementById("info-figu").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Sauvignon Blanc</p><p>Alkoholtartalom: 13,5%</p><p>Milyen ételhez ajánljuk:</p><p>fehér (szárnyas) húsok</p> ";
+
+}
+function szaszi(){
+    document.getElementById("info-szaszi").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Muscat Ottonel</p><p>Alkoholtartalom: 11%</p><p>Milyen ételhez ajánljuk:</p><p>desszertek mellé</p> ";
+
+} 
+function tussock(){
+    document.getElementById("info-tussock").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Sauvignon Blanc</p><p>Alkoholtartalom: 13,5%</p><p>Milyen ételhez ajánljuk:</p><p>fehér (szárnyas) húsok</p> ";
+
+} 
+function balla(){
+    document.getElementById("info-balla").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Királyleányka</p><p>Alkoholtartalom: 13%</p><p>Milyen ételhez ajánljuk:</p><p>fehér (szárnyas) húsok</p> ";
+
+}
+function laposa(){
+    document.getElementById("info-laposa").innerHTML = "<p>Évjárat: 2021</p><p>Szőlőfajta: fehér cuvée</p><p>Alkoholtartalom: 11,5%</p><p>Milyen ételhez ajánljuk:</p><p>desszertek mellé</p> ";
+
+}
+function krein(){
+    document.getElementById("info-krein").innerHTML = "<p>Évjárat: 2019</p><p>Szőlőfajta: Juhfark</p><p>Alkoholtartalom: 13%</p><p>Milyen ételhez ajánljuk:</p><p>halak, tenger gyümölcsei</p> ";
+
+}
+
+function nonefigu(){
+    document.getElementById("info-figu").innerHTML= "";
+}
+function noneszaszi(){
+    document.getElementById("info-szaszi").innerHTML= "";
+}
+function nonetussock(){
+    document.getElementById("info-tussock").innerHTML= "";
+}
+function noneballa(){
+    document.getElementById("info-balla").innerHTML= "";
+}
+function nonelaposa(){
+    document.getElementById("info-laposa").innerHTML= "";
+}
+function nonekrein(){
+    document.getElementById("info-krein").innerHTML= "";
+}
+
+//rosé
+function ballarose(){
+    document.getElementById("info-ballarose").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Rosé házasítás</p><p>Alkoholtartalom: 13%</p><p>Milyen ételhez ajánljuk:</p><p>Ázsiai ételek</p> ";
+
+}
+function stier(){
+    document.getElementById("info-stier").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Merlot</p><p>Alkoholtartalom: 12%</p><p>Milyen ételhez ajánljuk:</p><p>vöröshúsok</p> ";
+
+}
+function koch(){
+    document.getElementById("info-koch").innerHTML = "<p>Évjárat: 2021</p><p>Szőlőfajta: Rosé házasítás</p><p>Alkoholtartalom: 12%</p><p>Milyen ételhez ajánljuk:</p><p>Ázsiai ételek</p> ";
+
+}
+function lisicza(){
+    document.getElementById("info-lisicza").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Rosé házasítás</p><p>Alkoholtartalom: 11,5%</p><p>Milyen ételhez ajánljuk:</p><p>Ázsiai ételek</p> ";
+
+}
+function sauska(){
+    document.getElementById("info-sauska").innerHTML = "<p>Évjárat: 2019</p><p>Szőlőfajta: Rosé házasítás</p><p>Alkoholtartalom: 12,5%</p><p>Milyen ételhez ajánljuk:</p><p>Ázsiai ételek</p> ";
+
+}
+function frit(){
+    document.getElementById("info-frit").innerHTML = "<p>Évjárat: 2020</p><p>Szőlőfajta: Rosé házasítás</p><p>Alkoholtartalom: 12,5%</p><p>Milyen ételhez ajánljuk:</p><p>halak, tenger gyümölcsei</p> ";
+
+}
+
+function noneballarose(){
+    document.getElementById("info-ballarose").innerHTML= "";
+}
+function nonestier(){
+    document.getElementById("info-stier").innerHTML= "";
+}
+
+function nonekoch(){
+    document.getElementById("info-koch").innerHTML= "";
+}
+
+function nonelisicza(){
+    document.getElementById("info-lisicza").innerHTML= "";
+}
+function nonesauska(){
+    document.getElementById("info-sauska").innerHTML= "";
+}
+
+function nonefrit(){
+    document.getElementById("info-frit").innerHTML= "";
+}
+
+function summon(){
+    var fullcost = 0;
+    
+    //VÖRÖSBOROK 
+    var count1 = document.getElementById("count-vida").value;
+    var count2 = document.getElementById("count-meszaros").value;
+    var count3 = document.getElementById("count-sauskabor").value;
+    var count4 = document.getElementById("count-gere").value;
+    var count5 = document.getElementById("count-bonsai").value;
+    var count6 = document.getElementById("count-aldas").value;
+
+    //FEHÉRBOROK
+    var count7 = document.getElementById("count-figu").value;
+    var count8 = document.getElementById("count-szaszi").value;
+    var count9 = document.getElementById("count-tussock").value;
+    var count10 = document.getElementById("count-balla").value;
+    var count11 = document.getElementById("count-laposa").value;
+    var count12 = document.getElementById("count-krein").value;
+
+    //ROSÉBOROK
+    var count13 = document.getElementById("count-ballarose").value;
+    var count14 = document.getElementById("count-stier").value;
+    var count15 = document.getElementById("count-koch").value;
+    var count16 = document.getElementById("count-lisicza").value;
+    var count17 = document.getElementById("count-sauska").value;
+    var count18 = document.getElementById("count-frit").value;
+
+
+
+    //VÖRÖSBOROK
+    if(document.getElementById("vida").checked){
+        fullcost = fullcost + 2250*count1;
+    }
+    if(document.getElementById("meszaros").checked){
+        fullcost = fullcost + 4190*count2;
+    }
+    if(document.getElementById("sauskabor").checked){
+        fullcost = fullcost + 2790*count3;
+    }
+    if(document.getElementById("gere").checked){
+        fullcost = fullcost + 4750*count4;
+    }
+    if(document.getElementById("bonsai").checked){
+        fullcost = fullcost + 3260*count5;
+    }
+    if(document.getElementById("aldas").checked){
+        fullcost = fullcost + 3750*count6;
+    }
+
+    //FEHÉRBOROK
+    if(document.getElementById("figu").checked){
+        fullcost = fullcost + 2450*count7;
+    }
+    if(document.getElementById("szaszi").checked){
+        fullcost = fullcost + 1750*count8;
+    }
+    if(document.getElementById("tussock").checked){
+        fullcost = fullcost + 3910*count9;
+    }
+    if(document.getElementById("balla").checked){
+        fullcost = fullcost + 2410*count10;
+    }
+    if(document.getElementById("laposa").checked){
+        fullcost = fullcost + 1930*count11;
+    }
+    if(document.getElementById("krein").checked){
+        fullcost = fullcost + 2590*count12;
+    }
+
+    //ROSÉBOROK
+    if(document.getElementById("ballarose").checked){
+        fullcost = fullcost + 2290*count13;
+    }
+    if(document.getElementById("stier").checked){
+        fullcost = fullcost + 1790*count14;
+    }
+    if(document.getElementById("koch").checked){
+        fullcost = fullcost + 1820*count15;
+    }
+    if(document.getElementById("lisicza").checked){
+        fullcost = fullcost + 1450*count16;
+    }
+    if(document.getElementById("sauska").checked){
+        fullcost = fullcost + 2150*count17;
+    }
+    if(document.getElementById("frit").checked){
+        fullcost = fullcost + 1980*count18;
+    }
+
+    document.getElementById("summon").innerHTML = fullcost + 1100 + " Ft.";
+}
+
+function resetbasket(){
+    document.getElementById("summon").innerHTML = "0 Ft.";
+}
+
